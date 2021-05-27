74efddef6b08f60b2715099a1746e6e63aeedc42
Path-may1 <mayurapathak1@gmail.com>
2021-05-26 20:20:01 -0400

    recursive functions

---
+++ AccessingObjectPropertiesWithVariablesPassed.js
@@ -0,0 +1,12 @@
+// Accessing Object Properties with VariablesPassed
+
+let testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+
+
+let playerNumber = 16  
+let player = testObj[playerNumber];   
